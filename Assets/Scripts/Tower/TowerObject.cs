781b87f378916e8d12a3515ad70d217472557123
HOHOJO <ckwhs95@gmail.com>
2023-09-25 18:40:04 +0900

    테스트용 타워 만들기

--- Assets/Scripts/Tower/TowerObject.cs
+++ Assets/Scripts/Tower/TowerObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,28 +8,56 @@ public class TowerObject : MonoBehaviour
 {
     public TowerData data;
     public int index;
-    public Image sprite;
+    public Sprite sprite;
+    public GameObject bullet;
     private TowerSlot curSlot;
     public bool dead;
+    private Transform bulletSpawnPoint;
+    private List<GameObject> coll = new List<GameObject>();
+    private float timer = 0.0f;
+    private int waitingTime = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        bulletSpawnPoint = this.transform.Find("BulletSpawnPoint");
+        waitingTime = 2;
+        timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        timer += Time.deltaTime;
+        
     }
 
     public void Attack()
     {
+        if(timer>waitingTime)
+        {
+            Instantiate<GameObject>(this.bullet, this.bulletSpawnPoint.position, Quaternion.identity);
+            timer = 0f;
+        }
 
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("만남");
+        if (collision.tag == "Enemy")
+        {
+            Attack();
+            Debug.Log("만남");
+        }
+    }
+
     public void Production()
+    {
+
+    }
+
+    public void Dead()
     {
 
     }
