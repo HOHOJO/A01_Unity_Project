8aaad3912f8e6e30e48a0756fb3e2a39ae643e03
HOHOJO <ckwhs95@gmail.com>
2023-09-25 16:31:09 +0900

    타워기본(깡통)

---
+++ Assets/Scripts/Tower/TowerObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerObject : MonoBehaviour
+{
+    public TowerData data;
+    public int index;
+    public Image sprite;
+    private TowerSlot curSlot;
+    public bool dead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Production()
+    {
+
+    }
+
+    public void OnButtonClick()
+    {
+        
+    }
+}
